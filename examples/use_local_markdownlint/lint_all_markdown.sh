79d61dd6feb566b33d855253810ce2a4962484d2
David Johnston <dwtj@dwtj.me>
2020-07-02 15:56:08 -0700

    WIP: Add `--output_groups=...` to example

--- examples/use_local_markdownlint/lint_all_markdown.sh
+++ examples/use_local_markdownlint/lint_all_markdown.sh
@@ -1,4 +1,3 @@
 #!/bin/sh -
 
-echo 'TODO(dwtj)'
-#bazel build ... --aspects @me_dwtj_rules_markdown//markdown:defs.bzl%markdown_lint_aspect
+bazel build ... --aspects @me_dwtj_rules_markdown//markdown:defs.bzl%markdown_lint_aspect --output_groups=markdownlint_logs
